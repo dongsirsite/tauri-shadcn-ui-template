c576ced9924ba4722d33eac398354443b5c55ea9
dongsirhub <dwx_job@163.com>
2025-05-26 19:40:26 +0800

    build: 更新项目配置和依赖
    - 新增 ESLint 配置文件，规范代码风格
    - 更新项目标题为 "Vite + React + TS"
    - 在 package.json 中添加 lint 脚本和新依赖
      - ESLint 相关依赖
      - Tailwind CSS
      - React Refresh
    - 更新 TypeScript 依赖版本

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// @ts-expect-error process is a nodejs global
+import react from "@vitejs/plugin-react-swc";
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vitejs.dev/config/
-export default defineConfig(async () => ({
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [react()],
-
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
@@ -29,4 +26,4 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
-}));
+});
