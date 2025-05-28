9b09ee203618017ea372cfb545be5f12f86a1621
dongsirhub <dwx_job@163.com>
2025-05-28 20:31:36 +0800

    refactor(layout): 优化布局结构并移除无用代码
    - 重构 Layout 组件，使用 flex 布局并添加分隔线
    - 移除 App 组件中未使用的图片和 CSS 导入
    - 清理不必要的 logo 展示代码

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,17 @@
 import { Link, Outlet } from "react-router";
+import { Separator } from "./components/ui/separator";
 
 export default function Layout() {
   return (
-    <div>
+    <div className="h-screen w-screen flex flex-col">
+      <div className="flex flex-row">
         <Link to="/">Home</Link>
+        <Separator orientation="vertical" />
         <Link to="/app">App</Link>
-      <h1>Parent Content</h1>
-      <Outlet />
+      </div>
+      <div className="flex-1">
+        <Outlet />
+      </div>
     </div>
   );
 }
