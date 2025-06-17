3469a35c0642561445ae7dd6046bb3bcf14807d2
dongsirhub <dwx_job@163.com>
2025-06-17 22:23:06 +0800

    style(tasks1): 优化数据表格组件样式
    - 为外部 div 添加 height: 100% 样式，确保表格高度充满容器
    - 在内部 div 添加 overflow-x-auto 样式，允许水平滚动

--- src/page/tasks1/components/data-table.tsx
+++ src/page/tasks1/components/data-table.tsx
@@ -73,9 +73,9 @@ export function DataTable<TData, TValue>({
   })
 
   return (
-    <div className="flex flex-1 flex-col">
+    <div className="flex flex-1 flex-col h-full">
       <DataTableToolbar table={table} />
-      <div className="flex-1 overflow-y-auto">
+      <div className="flex-1 overflow-y-auto overflow-x-auto">
         <Table className="table-fixed">
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
